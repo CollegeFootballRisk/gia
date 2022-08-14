b49cced0623146fb74c2ea18a0da28e8aae39814
Mautamu <mautam@usa.com>
2022-08-14 02:48:32 -0500

    Awards

--- src/routes/player.svelte
+++ src/routes/player.svelte
@@ -3,6 +3,10 @@
 import { getPlayer } from "../utils/loads";
 import Loader from "../components/Loader.svelte";
 import { normalizeTeamName } from "../utils/normalization";
+import { fa1, fa2, fa3, faAward, faJedi, faShield, faStroopwafel } from "@fortawesome/free-solid-svg-icons";
+import {
+    FontAwesomeIcon,
+} from 'fontawesome-svelte';
 
     export var currentRoute;
     export var params;
@@ -35,6 +39,31 @@ import { normalizeTeamName } from "../utils/normalization";
   <h1>{player.name}</h1>
   <h2 style:text-shadow={`0px 0px 5px var(--${normalizeTeamName(player.team.name)}-secondary)`} style:color={`var(--${normalizeTeamName(player.team.name)}-primary)`}>{String.fromCharCode(0x272F).repeat(player.ratings.overall)}</h2>
   <h4><i>{(player.team.name == player.active_team.name)? player.team.name : `${player.team.name}, playing for ${(player.active_team.name == null)?'Undecided':player.active_team.name}`}</i></h4>
+  {#if player.awards.length != 0}
+  <center>
+    <hr />
+    <div class="lrow lrowthin">
+        {#each player.awards as award}
+        <div class="lcol">
+                {#if award.name == "Developer"}
+                <b title={award.info}><FontAwesomeIcon icon={faJedi}  />&nbsp; {award.name}</b>
+                    {:else if award.name == "Moderator"}
+                    <b title={award.info}><FontAwesomeIcon icon={faShield}  />&nbsp; {award.name}</b>
+                    {:else if award.name == "1"}
+                    <b title={award.info}><FontAwesomeIcon icon={fa1}  /></b>
+                    {:else if award.name == "2"}
+                    <b title={award.info}><FontAwesomeIcon icon={fa2}  /></b>
+                    {:else if award.name == "2.1"}
+                    <b title={award.info}><FontAwesomeIcon icon={faStroopwafel}  />&nbsp; {award.name}</b>
+                    {:else}
+                    <b title={award.info}><FontAwesomeIcon icon={faAward}  />&nbsp; {award.name}</b>
+                {/if}
+        </div>
+        {/each}
+    </div>
+    <hr />
+  </center>
+  {/if}
   <center>
         <div class="lrow">
             <div class="lcol2">
@@ -63,16 +92,16 @@ import { normalizeTeamName } from "../utils/normalization";
   rows={player.turns}
   bind:sortBy
   bind:sortOrder
-  classNameTable={["table table-striped"]}
+  classNameTable={["table90 table-striped"]}
   classNameThead={["table-primary"]}
   classNameSelect={["custom-select"]}
-/>
+  />
 </center>
   
   {/await}
 
   <style>
-                .lrow {
+            .lrow {
                 display: flex;
                 justify-content: center;
             }
@@ -85,6 +114,9 @@ import { normalizeTeamName } from "../utils/normalization";
             .lcol {
                 flex: 1;
                 flex-basis: 33%;
+            }
+            .lrowthin {
+                width: 50%;
             }
     h1 {
         padding-top: 10px;
