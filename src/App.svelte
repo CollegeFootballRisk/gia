863e33f4a0848d1bd3907266b8659a18889fe939
Mautamu <mautam@usa.com>
2023-01-22 23:08:40 +0000

    Fix login bug

--- src/App.svelte
+++ src/App.svelte
@@ -84,7 +84,7 @@
             <a href="/player/{$user.name}" on:click={showMe}>{$user.name}</a>
           </li>
         {:else}
-          <a href="/login/reddit" target="_blank">Login</a>
+          <li><a href="/login/reddit" target="_blank">Login</a></li>
         {/if}
       {/key}
       <li>
